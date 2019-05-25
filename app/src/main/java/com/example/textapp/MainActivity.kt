165e107c777d256ec369b451d8c7b39ede359c2b
Soul <soulctf@gmail.com>
2019-05-25 13:58:06 -0400

    WifiDirect requirements

--- app/src/main/java/com/example/textapp/MainActivity.kt
+++ app/src/main/java/com/example/textapp/MainActivity.kt
@@ -2,13 +2,18 @@ package com.example.textapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
 
     }
 }
--- app/src/main/java/com/example/textapp/WiFiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/textapp/WiFiDirectBroadcastReceiver.kt
@@ -4,20 +4,25 @@ import android.net.wifi.p2p.WifiP2pManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.TextView
+
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
  */
 class WiFiDirectBroadcastReceiver(
     private val manager: WifiP2pManager,
     private val channel: WifiP2pManager.Channel,
-    private val activity: MainActivity,
-) : BroadcastReceiver() {
+    private val activity: MainActivity
+) : BroadcastReceiver()  {
 
     override fun onReceive(context: Context, intent: Intent) {
         val action: String = intent.action
         when (action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-
+                    val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                    if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    } else {
+                    }
             }
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
@@ -27,23 +32,6 @@ class WiFiDirectBroadcastReceiver(
             }
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
                 // Respond to this device's wifi state changing
-            }
-        }
-    }
-
-    override fun onReceive(context: Context, intent: Intent) {
-        val action: String = intent.action
-        when (action) {
-            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
-                when (state) {
-                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
-                        // Wifi P2P is enabled
-                    }
-                    else -> {
-                        // Wi-Fi P2P is not enabled
-                    }
-                }
             }
         }
     }
