00b13f01652429cd1343d3bbb929bf96102476e9
Soul <soulctf@gmail.com>
2019-05-25 13:01:20 -0400

    WifiDirect requirements

--- app/src/main/java/com/example/textapp/WiFiDirectBroadcastReceiver.kt
+++ app/src/main/java/com/example/textapp/WiFiDirectBroadcastReceiver.kt
@@ -1,19 +1,23 @@
 package com.example.textapp
 
+import android.net.wifi.p2p.WifiP2pManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
  */
 class WiFiDirectBroadcastReceiver(
     private val manager: WifiP2pManager,
     private val channel: WifiP2pManager.Channel,
-    private val activity: MyWifiActivity
+    private val activity: MainActivity,
 ) : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val action: String = intent.action
         when (action) {
             WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                // Check to see if Wi-Fi is enabled and notify appropriate activity
+
             }
             WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
                 // Call WifiP2pManager.requestPeers() to get a list of current peers
@@ -23,6 +27,23 @@ class WiFiDirectBroadcastReceiver(
             }
             WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
                 // Respond to this device's wifi state changing
+            }
+        }
+    }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val action: String = intent.action
+        when (action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                when (state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                    }
+                    else -> {
+                        // Wi-Fi P2P is not enabled
+                    }
+                }
             }
         }
     }
