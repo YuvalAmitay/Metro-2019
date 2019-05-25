6383867fa560f84b5cae206a1eed68bb66e1f04a
YuvalAmitay <yuvalamiaty42@gmail.com>
2019-05-25 14:38:11 -0400

    change (run1)

--- app/src/main/java/com/example/textapp/ScrollingActivity.kt
+++ app/src/main/java/com/example/textapp/ScrollingActivity.kt
@@ -22,7 +22,7 @@ class ScrollingActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_scrolling, menu)
-        return true
+        return trues
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
